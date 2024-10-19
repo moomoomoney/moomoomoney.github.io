2bec61449f3d257376994086227770aeb0058a75
moomoomoney <santaclausofficialho@gmail.com>
2024-10-19 16:56:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Update Homescreen
+var coins = "160 M³"  // Available Coins
+var income = "28 M³"; // Today's Income
+var losses = "0 M³"; //Today's losses
+var auto = "0 M³";   //Auto Earnings
+
+// Get the elements by class name
+const coinsElement = document.getElementsByClassName("coins")[0];
+const incomeElement = document.getElementsByClassName("income")[0];
+const lossesElement = document.getElementsByClassName("losses")[0];
+const autoElement = document.getElementsByClassName("auto")[0];
+
+// Update the inner HTML of each element
+coinsElement.innerHTML = coins;
+incomeElement.innerHTML = income;
+lossesElement.innerHTML = losses;
+autoElement.innerHTML = auto;
+
+function reveal(id) {
+    const element = document.getElementById(id);
+    element.style.display = element.style.display === "none" ? "" : "none";
+  }
